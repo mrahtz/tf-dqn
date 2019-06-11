ee3682b8495729f6799bfb2bace2bfe4fcaa0fd0
Matthew Rahtz <matthew.rahtz@gmail.com>
2019-06-11 16:27:05 +0200

    Clean up tests

--- tests.py
+++ tests.py
@@ -22,45 +22,30 @@ class Tests(unittest.TestCase):
     def test_experience_buffer(self):
         buf = ReplayBuffer(obs_shape=(), max_size=2)
 
-        self.store_in_buf(buf, 1)
-        self.check_buf_equals(buf, [1])
+        store_in_buf(buf, 1)
+        check_buf_equals(buf, [1])
 
-        self.store_in_buf(buf, 2)
-        self.check_buf_equals(buf, [1, 2])
+        store_in_buf(buf, 2)
+        check_buf_equals(buf, [1, 2])
 
-        self.store_in_buf(buf, 3)
-        self.check_buf_equals(buf, [2, 3])
-
-    def check_buf_equals(self, buf, values):
-        samples = {buf.sample(batch_size=1).obs1[0] for _ in range(100)}
-        self.assertEqual(samples, set(values))
-
-    def store_in_buf(self, buf, value):
-        buf.store(obs1=value, acts=None, rews=None, obs2=None, done=None)
+        store_in_buf(buf, 3)
+        check_buf_equals(buf, [2, 3])
 
     def test_model_train(self):
-        model = Model(obs_shape=(1,), n_actions=2, lr=1e-3, seed=0, discount=0.99, policy_fn=MLPPolicy)
-        obs1 = [np.random.rand()]
-        obs2 = [np.random.rand()]
-        act = 0
-        rew = 1
+        model = get_model()
+        (obs1, obs2), act, rew = np.random.rand(2, 1), 0, 1
         batch = ReplayBatch(obs1=[obs1], acts=[act], rews=[rew], obs2=[obs2], done=[True])
         for _ in range(200):
             model.train(batch)
-        assert_approx_equal(model.q(obs1, act), rew, significant=3)
+        expected_q = rew
+        actual_q = model.q(obs1, act)
+        assert_approx_equal(actual_q, expected_q, significant=3)
 
     def test_model_update_target(self):
-        model = Model(obs_shape=(1,), n_actions=2, seed=0, discount=0.99, lr=1e-3, policy_fn=MLPPolicy)
-        obs = [np.random.rand()]
-
-        q1s, q2s = model.sess.run([model.q1s_main, model.q2s_target], feed_dict={model.obs1_ph: [obs],
-                                                                                 model.obs2_ph: [obs]})
-        assert_raises(AssertionError, assert_allclose, q1s, q2s)
-
+        model = get_model()
+        check_main_target_different(model)
         model.update_target()
-        q1s, q2s = model.sess.run([model.q1s_main, model.q2s_target], feed_dict={model.obs1_ph: [obs],
-                                                                                 model.obs2_ph: [obs]})
-        assert_allclose(q1s, q2s)
+        check_main_target_same(model)
 
     def test_tensor_index(self):
         sess = tf.Session()
@@ -86,6 +71,35 @@ class EndToEnd(unittest.TestCase):
                 rewards.append(reward)
             episode_rewards.append(sum(rewards))
         self.assertEqual(min(episode_rewards), 200)
+
+
+def check_main_target_different(model):
+    obs = [np.random.rand()]
+    q1s, q2s = model.sess.run([model.q1s_main, model.q2s_target], feed_dict={model.obs1_ph: [obs],
+                                                                             model.obs2_ph: [obs]})
+    assert_raises(AssertionError, assert_allclose, q1s, q2s)
+    return obs
+
+
+def check_main_target_same(model):
+    obs = [np.random.rand()]
+    q1s, q2s = model.sess.run([model.q1s_main, model.q2s_target], feed_dict={model.obs1_ph: [obs],
+                                                                             model.obs2_ph: [obs]})
+    assert_allclose(q1s, q2s)
+
+
+def get_model():
+    model = Model(obs_shape=(1,), n_actions=2, lr=1e-3, seed=0, discount=0.99, policy_fn=MLPPolicy)
+    return model
+
+
+def check_buf_equals(buf, values):
+    samples = {buf.sample(batch_size=1).obs1[0] for _ in range(100)}
+    assert samples == set(values)
+
+
+def store_in_buf(buf, value):
+    buf.store(obs1=value, acts=None, rews=None, obs2=None, done=None)
 
 
 if __name__ == '__main__':

8a1f91bb07043d604fade8da4601c242820d1c9b
Matthew Rahtz <matthew.rahtz@gmail.com>
2019-07-19 11:56:24 +0200

    Fix Atari hyperparameters

--- dqn/config.py
+++ dqn/config.py
@@ -1,11 +1,12 @@
+# https://github.com/hill-a/stable-baselines/blob/master/stable_baselines/deepq/dqn.py
 default_config = dict(
     batch_size=32,
-    gamma=1.0,
+    gamma=0.99,
     random_action_prob=0.1,
-    n_start_steps=1000,
+    n_start_steps=1_000,
     update_target_every_n_steps=500,
     log_every_n_steps=100,
-    checkpoint_every_n_steps=1000,
+    checkpoint_every_n_steps=1_000,
     n_env_steps_per_rl_update=1,
     buffer_size=50_000,
     lr=5e-4,
@@ -15,20 +16,18 @@ default_config = dict(
     seed=0,
     double_dqn=True,
     dueling=True,
-    prioritized=True,
+    prioritized=False,
     features='mlp',
     gradient_clip=10,
     async_test=False,
-    test_every_n_steps=5000,
+    test_every_n_steps=5_000,
 )
 
-# Hyperparams from
-# https://github.com/hill-a/stable-baselines/blob/72dab6a37bb33ad959cc598f187299edc49c425f/stable_baselines/deepq/dqn.py
+# https://github.com/hill-a/stable-baselines/blob/master/stable_baselines/deepq/experiments/run_atari.py
 atari_config = dict(
-    gamma=0.99,
-    n_start_steps=1_000,
-    update_target_every_n_steps=500,
-    n_env_steps_per_rl_update=1,
-    features='cnn',
-    train_n_steps=1e7
+    lr=1e-4,
+    buffer_size=10_000,
+    update_target_every_n_steps=1_000,
+    n_start_steps=10_000,
+    n_env_steps_per_rl_update=4,
 )

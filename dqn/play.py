20a182a6ce244c2783e3edebe62aa6b372482a0f
Matthew Rahtz <matthew.rahtz@gmail.com>
2019-07-23 19:37:22 +0200

    Add play.py

---
+++ dqn/play.py
@@ -0,0 +1,26 @@
+import argparse
+import time
+from functools import partial
+
+from dqn.env import make_env
+from dqn.model import Model
+from dqn.policies import CNNFeatures, Policy
+
+parser = argparse.ArgumentParser()
+parser.add_argument('ckpt_dir')
+args = parser.parse_args()
+
+env = make_env('PongNoFrameskip-v4', seed=0)
+
+policy_fn = partial(Policy, features_cls=CNNFeatures, dueling=False)
+model = Model(policy_fn=policy_fn, obs_shape=env.observation_space.shape, n_actions=env.action_space.n,
+              discount=0.99, lr=1e-5, gradient_clip=10, seed=0, double_dqn=False)
+model.load(args.ckpt_dir)
+
+while True:
+    done, obs = False, env.reset()
+    while not done:
+        action = model.step(obs, random_action_prob=0)
+        obs, reward, done, _ = env.step(action)
+        env.render()
+        time.sleep(1/60)
